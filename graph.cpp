f60acc0903f01b039d8e26d2a8e9dac184b0390c
SUPERKK11 <123571175+SUPERKK11@users.noreply.github.com>
2024-04-05 19:03:49 +0530

    Update graph.cpp

--- graph.cpp
+++ graph.cpp
@@ -1,39 +1,64 @@
-// this code is based on adjency list representation of graph 
 #include<iostream>
 using namespace std;
-
-class graph {
-public:
+class graph{
+    public:
     int data;
-    int weight;
-    graph* next;
+    graph*next;
+
 };
+graph*adj_list[100];
+void add_vertex(int vertex){
+     graph*newnode=new graph();
+     newnode->data=vertex;
+     newnode->next=NULL;
+     adj_list[vertex]=newnode;
 
-graph* arr[100];
-
-void add_vertex(int vertex) {
-    graph* newnode = new graph();
-    newnode->data = vertex;
-    newnode->next = nullptr;
-    arr[vertex] = newnode;
 }
-
-void add_edge(int vertex1, int vertex2, int weight, bool isDirected) {
-    graph* newnode1 = new graph();
-    newnode1->data = vertex2;
-    newnode1->weight = weight;
-    newnode1->next = arr[vertex1]->next;
-    arr[vertex1]->next = newnode1;
-
-    if (!isDirected) {
-        graph* newnode2 = new graph();
-        newnode2->data = vertex1;
-        newnode2->weight = weight;
-        newnode2->next = arr[vertex2]->next;
-        arr[vertex2]->next = newnode2;
+void add_edges(int vertex1 ,int vertex2, bool isdirected){
+    graph*newnode1=new graph();
+    newnode1->data=vertex2;
+    newnode1->next=adj_list[vertex1]->next;
+    adj_list[vertex1]->next=newnode1;
+    if(!isdirected){
+        graph*newnode2= new graph();
+        newnode2->data=vertex1;
+        newnode2->next=adj_list[vertex2]->next;
+        adj_list[vertex2]->next=newnode2;
     }
 }
+void DFSutil(int vertex,bool visited[]){
+// mark the current vertex as visited
+visited[vertex]=true;
+cout<<vertex<<"";
+//traverse all the vertices adjacent to this vertex
+graph*currentnode=adj_list[vertex];
+while(currentnode!=NULL){
+int adjacentVertex=currentnode->data;
+if(!visited[adjacentVertex]){
+    DFSutil(adjacentVertex,visited);
+}
+currentnode=currentnode->next;
 
-int main() {
+}
+
+}
+void DFS(int vertex){
+    //mark all vertices as not visited
+    bool visited[100]={false};
+    //call the recursive utility function to perform dfs traversal
+    DFSutil(vertex,visited);
+
+}
+int main(){
+    add_vertex(0);
+    add_vertex(1);
+    add_vertex(2);
+    add_vertex(3);
+    
+    add_edges(0,1,false);
+    add_edges(1,2,false);
+    add_edges(2,3,false);
+    add_edges(3,0,false);
+    DFS(0);
     return 0;
 }
